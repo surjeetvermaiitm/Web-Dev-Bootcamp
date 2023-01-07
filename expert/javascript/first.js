eef83477a5219415a4e0d25313a7dedd293a31ba
surjeetvermaiitm <surjeetverma2017@gmail.com>
2023-01-07 23:21:47 +0530

    html

---
+++ expert/javascript/first.js
@@ -0,0 +1,26 @@
+let num = "10px";
+num = 10.8;
+console.log(Number(num));
+console.log(parseInt(num));
+console.log(typeof -Infinity);
+console.log(typeof 100n);
+let str = "abcd";
+console.log(str);
+console.log(typeof str);
+console.log(str + 9);
+
+console.log(`Hello 
+Surjeet`);
+let myName = `surjeet
+kumar 
+      Verma`;
+console.log(myName);
+console.log(myName[0]);
+console.log(myName.charAt(1));
+console.log(myName.startsWith("s"));
+console.log(myName.endsWith("d"));
+console.log(myName.includes("sur"));
+console.log(myName.toUpperCase());
+console.log(myName.toLowerCase());
+console.log(myName.substr(1, 3));
+console.log(myName.includes("sur"));
