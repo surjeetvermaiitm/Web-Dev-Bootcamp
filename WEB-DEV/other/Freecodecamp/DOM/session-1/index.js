3f26bc6f17e5aee4dee75f452ab60dd23baf9996
surjeetvermaiitm <surjeetverma2017@gmail.com>
2023-01-22 21:20:48 +0530

    animation

---
+++ WEB-DEV/other/Freecodecamp/DOM/session-1/index.js
@@ -0,0 +1,52 @@
+// console.log("Hi");
+// var h = prompt("Hello");
+// console.log(h);
+//getElementById("idname")
+
+// document.getElementById("main-heading");
+
+// console.log(document.documentElement);
+// console.log(document.body);
+// console.log(document.head);
+
+//children
+// console.log(document.body.childNodes);
+// for (node of document.body.children) {
+//   console.log(node);
+// }
+// for (node of document.body.childNodes) {
+//   console.log(node);
+// }
+// console.log(document.body.childElementCount);
+// console.log(document.body.children);
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+// console.log(document.body.firstElementChild);
+// console.log(document.body.lastElementChild);
+
+// const childrensOfBody = Array.from(document.body.childNodes);
+// console.log(childrensOfBody);
+
+//siblings
+// console.log(document.body.children);
+
+// const ulTag = document.body.children[0];
+// console.log(ulTag);
+// console.log(ulTag.nextSibling);
+// console.log(ulTag.nextElementSibling);
+// const firstLi = ulTag.children[0];
+// const secondLi = ulTag.children[1];
+// console.log(secondLi.previousElementSibling);
+// console.log(secondLi.previousElementSibling.textContent);
+
+//Table
+// const tableTag = document.body.children[1];
+// console.log(tableTag);
+// console.log(tableTag.tBodies[0].rows);
+// console.log(tableTag.rows);
+// tableTag.tBodies[0].rows[0].cells[1].style = "background-color:red";
+
+//ID
+// console.log(ulid);
+// ulid.style.background = "red";
+ulid.style = "background-color:red";
