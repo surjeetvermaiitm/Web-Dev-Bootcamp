3948fef810919ea94812571a24e29dbabae81d2c
surjeetvermaiitm <surjeetverma2017@gmail.com>
2023-04-22 10:16:03 +0530

    add

--- WEB-DEV/ReactJS/Projects/03-top-courses-app/src/components/Navbar.jsx
+++
@@ -1,7 +0,0 @@
-export default function Navbar() {
-  return (
-    <nav className="bg-bgDark py-4">
-      <h1 className="text-3xl font-bold text-center text-white">Top Courses</h1>
-    </nav>
-  );
-}
--- WEB-DEV/ReactJS/Projects/03-top-courses-app/src/components/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-export default function Spinner() {
-  return (
-    <div className="flex flex-col items-center space-y-2">
-      {/* https://10015.io/tools/css-loader-generator */}
-      <div className="spinner"></div>
-      <p className="text-bgDark text-lg font-semibold">Loading ...</p>
-    </div>
-  );
-}
